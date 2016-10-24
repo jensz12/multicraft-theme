6b3d239866e7116686379629d6d2ff3d91eb372d
Jens Møller <jens@jensz12.com>
2016-10-24 15:38:28 +0200

    Add files via upload

---
+++ views/layouts/components/banner.php
@@ -0,0 +1,5 @@
+        <div class="row" id="header">
+            <div id="logo">
+                <h1>Multicraft <small>Minecraft panel</small></h1>
+            </div>
+        </div><!-- header -->
---
+++ views/layouts/components/head.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ *   Copyright © 2010-2016 by xhost.ch GmbH
+ *
+ *   All rights reserved.
+ *
+ **/
+?>
+<!doctype html>
+<html lang="en">
+<!--
+ -
+ -   Copyright © 2010-2016 by xhost.ch GmbH
+ -
+ -   All rights reserved.
+ -
+ -->
+<head>
+    <meta content="initial-scale=1.0, width=device-width, user-scalable=yes" name="viewport">
+    <meta content="yes" name="apple-mobile-web-app-capable">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
+    <link rev="made" href="mailto:jens@jensz12.com">
+    <meta name="description" content="Multicraft: The Minecraft server control panel">
+    <meta name="keywords" content="Multicraft, Minecraft, server, management, control panel, hosting">
+    <meta name="author" content="xhost.ch GmbH">
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="https://jensz12.com/favicon.png" />
+	<meta name="theme-color" content="#A81010">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo Theme::css('style.css') ?>" media="screen, projection" />
+
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+<body>
+<div id="page">
